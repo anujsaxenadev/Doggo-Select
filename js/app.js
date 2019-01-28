696b6a75259834aa4133af5ba85d279c60624fde
Anuj <beaver@Beaver>
2019-01-28 10:50:42 +0530

    getting breeds list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 const select = document.getElementById('breeds');
 const card = document.querySelector('.card'); 
 const form = document.querySelector('form');
+
+//Getting Breeds List.
+fetch('https://dog.ceo/api/breeds/list')
+    .then(response => response.json())
+    .then(data => data.message);
