a3066d1048ce6e7f206bfa05660527ee2fb0f4d5
Anuj <beaver@Beaver>
2019-01-28 11:42:53 +0530

    fetching breed image

--- js/app.js
+++ js/app.js
@@ -5,4 +5,17 @@ const form = document.querySelector('form');
 //Getting Breeds List.
 fetch('https://dog.ceo/api/breeds/list')
     .then(response => response.json())
-    .then(data => data.message);
+    .then(data => settingOptions(data.message))
+    .then(fetchDogImage);
+
+function fetchDogImage(){
+    fetch(`https://dog.ceo/api/breed/${select.value}/images/random`)
+        .then(image => image.json())
+        .then((url) => {card.innerHTML = `<img src="${url.message}"></img>`;});
+}
+
+function settingOptions(data){
+    //Generating Breeds
+    const options = data.map(breed => `<option>${breed}</option>`).join('');
+    select.innerHTML = options;
+}
