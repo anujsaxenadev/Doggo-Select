db42439339c548e1a622961d09c52fe614ba73fb
Anuj <beaver@Beaver>
2019-01-30 10:58:09 +0530

    Modularizing code and Error Handeling

--- js/app.js
+++ js/app.js
@@ -2,28 +2,44 @@ const select = document.getElementById('breeds');
 const card = document.querySelector('.card'); 
 const form = document.querySelector('form');
 
-//Getting Breeds List.
-fetch('https://dog.ceo/api/breeds/list')
-    .then(response => response.json())
-    .then(data => settingOptions(data.message))
-    .then(fetchDogImage);
-
-function fetchDogImage(){
-    fetch(`https://dog.ceo/api/breed/${select.value}/images/random`)
-        .then(image => image.json())
-        .then((url) => {
-            card.innerHTML = `
-                                <img src="${url.message}"></img>
-                                <p>Click to view more Images of <b>${select.value}</b></p>
-                            `;
-        });
+function fetchData(url){
+    return fetch(url)
+        .then(checkStatus)
+        .then(response => response.json())
+        .catch(error => console.log(`Looks like there was an error! | ${error}`));
 }
 
-function settingOptions(data){
+fetchData('https://dog.ceo/api/breeds/list')
+    .then(data => settingBreeds(data.message))
+    .then(fetchBreedImage);
+
+function settingBreeds(data){
     //Generating Breeds
     const options = data.map(breed => `<option>${breed}</option>`).join('');
     select.innerHTML = options;
 }
 
-select.addEventListener('change', fetchDogImage);
-card.addEventListener('click', fetchDogImage);
+function checkStatus(response){
+    if(response.ok == true){
+        return Promise.resolve(response);
+    }
+    else{
+        return Promise.reject(new Error(response.statusText));
+    }
+}
+
+function fetchBreedImage(){
+    const breed = select.value;
+
+    fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then(data => {
+            let card = `
+                <img src="${data.message}" alt="${breed}"></img>
+                <p>Click to view more <b>${breed}s</b></p>
+            `;
+            document.querySelector(".card").innerHTML = card;
+        });
+}
+
+select.addEventListener('change', fetchBreedImage);
+card.addEventListener('click', fetchBreedImage);
